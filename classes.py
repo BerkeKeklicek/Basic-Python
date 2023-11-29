b36d8e5958a0b035951d9dd33fc0c4abf9ac612b
Berke Keklicek <149376908+BerkeKeklicek@users.noreply.github.com>
2023-11-29 18:43:03 +0300

    classes.py

--- classes.py
+++ classes.py
@@ -1,16 +1,40 @@
 
 class Matematik:
-    def topla(self,sayi1,sayi2):
-        return sayi1 + sayi2
+    def __init__(self,sayi1,sayi2):
+        self.sayi1 = sayi1
+        self.sayi2 = sayi2
+    def topla(self):
+        return self.sayi1 + self.sayi2
     
-    def cikar(self,sayi1,sayi2):
-        return sayi1 - sayi2
+    def cikar(self):
+        return self.sayi1 - self.sayi2
     
-    def carp(self,sayi1,sayi2):
-        return sayi1 * sayi2
+    def carp(self):
+        return self.sayi1 * self.sayi2
     
-    def bol(self,sayi1,sayi2):
-        return sayi1 / sayi2
+    def bol(self):
+        return self.sayi1 / self.sayi2
 
-matematik = Matematik()
-print("Toplam = " + str(matematik.topla(2,28)))
+matematik = Matematik(2,78)
+matematik2 = Matematik(3,76)
+print("Toplam = " + str(matematik2.topla()))
+
+#Property
+
+class Person:
+    def __init__(self,firstName,lastName,age):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        
+person1 = Person("Berke","Keklicek",18)
+print(person1.firstName)
+
+class Worker(Person):
+    def __init__(self,salary):
+        self.salary = salary
+    
+class Customer(Person):
+    def __init__(self,creditCardNumber):
+        self.creditCardNumber = creditCardNumber
+
