8e57a4da837bf61b7263014cefd42f8522f9084d
Berke Keklicek <149376908+BerkeKeklicek@users.noreply.github.com>
2023-11-29 18:43:40 +0300

    Add files via upload

---
+++ moduleTest.py
@@ -0,0 +1,20 @@
+
+
+#import mathematicsModule
+
+#mathematicsModule.topla(2,3)
+#mathematicsModule.carp(2,3)
+
+
+import mathematicsModule as mm
+
+mm.carp(3,4)
+
+
+print(mm.customer["firstName"])
+
+from mathematicsModule import topla
+topla(8,9)
+
+from mathematicsModule import customer
+print(customer["lastName"])
